bba1604bd24c7094d6763051abaf1f957396526e
Michael Kopf <michael.kopf@sealsystems.de>
2019-05-05 08:34:16 +0200

    Introduced row-expansion for item-details.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 
@@ -7,12 +8,26 @@ import { User, UserService } from './user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('rowExpansionTrigger', [
+      state('void', style({
+        transform: 'translateX(-10%)',
+        opacity: 0
+      })),
+      state('active', style({
+        transform: 'translateX(0)',
+        opacity: 1
+      })),
+      transition('* <=> *', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'td-primeng';
   users: User[];
   loading = false;
+  columns = ['firstName', 'lastName', 'email'];
 
   constructor(private userService: UserService) { }
 
